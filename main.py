5a1590425ddeae4be87068cfc7a1c4d42038d447
p99will <p99will@gmail.com>
2020-06-19 14:49:47 +1200

    added better code from googles AI training course

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 import tensorflow as tf
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow import feature_column
+from tensorflow.keras import layers
+from matplotlib import pyplot as plt
 
-class ezTF:
-    model = None
+class MyModel:
+    model = tf.keras.models.Sequential()
     hastrained = False
     nodes=0
     layers=0
     epoc = 0
     learningRate=0
 
-    def __init__(self,inputs,outputs,nodes,layers,epoc):
-        self.epoc = epoc
+    def __init__(self,inputs,outputs,nodes,layers):
         self.nodes = nodes
         self.layers = layers
-        self.model = tf.keras.Sequential()
         self.model.add(tf.keras.layers.Dense(inputs, activation='relu'))
         for i in (0,layers):
             self.model.add(tf.keras.layers.Dense(nodes, activation='relu'))
         self.model.add(tf.keras.layers.Dense(outputs,activation=tf.nn.softmax))
 
-    def compile(self):
-        self.model.compile(optimizer='adam',
-                      loss="sparse_categorical_crossentropy",
-                      metrics=['accuracy'])
+    def compile(self,my_metrics,learningRate):
+        self.learningRate = learningRate
+        self.model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=self.learningRate),
+              loss=tf.keras.losses.BinaryCrossentropy(),
+              metrics=my_metrics)
 
-    def train(self,training_data,training_labels):
+    def train(self, model, dataset, epochs, label_name)
+                batch_size=None, shuffle=True):
+        features = training_data
+        label = training_labels
+        history = model.fit(x=features, y=label, batch_size=batch_size,
+                      epochs=epochs, shuffle=shuffle)
         self.hastrained = True
         return self.model.fit(training_data,training_labels,epochs=self.epoc)
 
